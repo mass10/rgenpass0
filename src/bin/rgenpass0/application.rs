d4f1f5b9397a31b772c04997e9a048636f7da487
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-03-31 14:27:34 +0900

    Update application.rs

--- src/bin/rgenpass0/application.rs
+++ src/bin/rgenpass0/application.rs
@@ -3,25 +3,41 @@ use super::generator;
 use super::listener;
 use super::server;
 
-/// 実行
-pub fn run(request: &str) -> Result<(), Box<dyn std::error::Error>> {
-	// オプションによる振り分け
-	if request == "--server" {
-		// 自身がサーバーになって処理を終了する。
-		listener::run_as_server()?;
+pub struct Application;
 
-		return Ok(());
-	} else {
-		// サーバーに状況を問い合わせます。
-		let current_complexity = client::try_to_request_server();
-		if current_complexity == 0 {
-			// サーバーの起動を試みます。
-			// println!("[TRACE] サーバーの起動を試みます。");
-			server::spawn_server_process()?;
+impl Application {
+	/// 新しいインスタンスを返します。
+	///
+	/// ### Returns
+	/// `Application` の新しいインスタンス
+	pub fn new() -> Application {
+		Application {}
+	}
+
+	/// 実行
+	///
+	/// ### Arguments
+	/// * `request` 呼び出しオプション
+	pub fn run(&self, request: &str) -> Result<(), Box<dyn std::error::Error>> {
+		// オプションによる振り分け
+		if request == "--server" {
+			// 自身がサーバーになって処理を終了する。
+			let listener = listener::Listener::new();
+			listener.run_as_server()?;
+
+			return Ok(());
+		} else {
+			// サーバーに状況を問い合わせます。
+			let current_complexity = client::try_to_request_server();
+			if current_complexity == 0 {
+				// サーバーの起動を試みます。
+				// println!("[TRACE] サーバーの起動を試みます。");
+				server::spawn_server_process()?;
+			}
+			// 出力
+			generator::generate_password(current_complexity)?;
+
+			return Ok(());
 		}
-		// 出力
-		generator::generate_password(current_complexity)?;
-
-		return Ok(());
 	}
 }
